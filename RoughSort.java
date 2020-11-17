07f85da73a7451b7b57161e31b1692b84c699bee
Charles Forster <charlesforster@Charless-MacBook.local>
2020-11-17 08:46:14 -0500

    Initial Files

---
+++ RoughSort.java
@@ -0,0 +1,16 @@
+/*
+  implement and demonstrate a sorting algorithm that:
+  1. runs in linear time
+  2. roughly sorts an array
+  it doesn't have to sort it completely, it just needs to get closer to sorted than before.
+  how good can you get in linear time?
+*/
+
+
+public static void main(String[] args) {
+    System.out.println(Arrays.toString(roughSort(new int[] {5, 32, 1, 100, 20, 30})));
+}
+
+public static int[] roughSort(int[] unsorted) {
+    return unsorted;
+}
